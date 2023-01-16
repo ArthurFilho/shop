ce3794f874deabe212eb2e662afb00d1253712ad
Arthur <peraarthur2018@gmail.com>
2023-01-16 20:11:58 -0300

    styles

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,17 +1,18 @@
 import { HeaderContainer, ImgLogo, Logo, MenuItems, Search, SearchMenu } from "./styles";
 import LogoC from "../../assets/MainPage/logo.png"
+import { NavLink } from "react-router-dom";
+
 
 export function Header () {
     return(
         <HeaderContainer>
-        <Logo>
-            <ImgLogo src={LogoC} />
-        </Logo>
-
+            <Logo>
+               <NavLink to="/"> <ImgLogo src={LogoC} /> </NavLink>
+            </Logo>
         <MenuItems>
-            <div> Início </div>
-            <div> Catálogo </div>
-            <div> Entrar em contato </div>
+                <div> Início </div>
+                <div> Catálogo </div>
+                <div> Entrar em contato </div>
             <SearchMenu>
                 <Search type="text" placeholder="Pesquise" />
             </SearchMenu>
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,7 +5,6 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    overflow-x: hidden;
     }
 :focus {
       outline: 0;
@@ -18,5 +17,6 @@ body {
 }
 body, input-security, textarea, button {
     font: 400 1rem Roboto, sans-serif;
+    overflow-x: hidden;
 }
 `
